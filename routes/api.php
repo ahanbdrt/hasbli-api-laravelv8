0377352184cd6dfca17ebed60a6f769cca63cd12
ahanbdrt <ahanbudi9@gmail.com>
2022-03-25 15:22:12 +0700

    yes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/tenant', App\Http\Controllers\Api\TenantController::class);
