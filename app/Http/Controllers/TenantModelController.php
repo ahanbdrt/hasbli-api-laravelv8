62a7765d636dbe1dfc92a0eec2bba7254f52c2f5
suryo <suryoatm@gmail.com>
2022-03-25 14:26:26 +0700

    fixed endpoint get tenant

---
+++ app/Http/Controllers/TenantModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tenant_model;
+use Illuminate\Http\Request;
+
+class TenantModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tenant_model  $tenant_model
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tenant_model $tenant_model)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tenant_model  $tenant_model
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tenant_model $tenant_model)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tenant_model  $tenant_model
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tenant_model $tenant_model)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tenant_model  $tenant_model
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tenant_model $tenant_model)
+    {
+        //
+    }
+}
